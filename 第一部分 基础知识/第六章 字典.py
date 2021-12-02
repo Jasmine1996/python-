a785c85e5b4f8dea590401ee64aa63735a46f039
jasmine1996 <jasmine_ming@163.com>
2021-12-02 09:50:01 +0800

    2021-12-2 9:49

--- 第一部分 基础知识/第六章 字典.py
+++ 第一部分 基础知识/第六章 字典.py
@@ -4,3 +4,16 @@
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+
+
+# 6.2 使用字典
+
+# 6.2.2 添加键值对
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_positino'] = 25
+print(alien_0)
+
+# 6.2.3 先创建一个空字典
