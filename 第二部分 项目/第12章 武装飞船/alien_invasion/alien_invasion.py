520eb7a60224e2e1739003ed052e5d445ab0ecd0
jasmine1996 <jasmine_ming@163.com>
2021-12-18 14:03:12 +0800

    为重构run_game()方法之前

--- 第二部分 项目/第12章 武装飞船/alien_invasion/alien_invasion.py
+++ 第二部分 项目/第12章 武装飞船/alien_invasion/alien_invasion.py
@@ -1,17 +1,22 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        # 设置背景色
-        self.bg_color = (230, 230, 230)
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -22,8 +27,9 @@ class AlienInvasion:
                     sys.exit()
 
             # 每次循环时都重绘屏幕。
-            self.screen.fill(self.bg_color)
-            
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # 让最近绘制的屏幕可见。
             pygame.display.flip()
 
